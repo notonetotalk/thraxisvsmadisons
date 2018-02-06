2dcdc706e19f1a1d530458e19d1113036caeeec3
notonetotalk <rgatchal@gmail.com>
2018-02-05 22:14:01 -0800

    deploy version

--- tvmsim.dart
+++ tvmsim.dart
@@ -64,22 +64,20 @@ final Map stepsM = {
 };
 // Initalize variables for use in functions.
 final InputElement numInput = querySelector("#numInput");
-final CheckboxInputElement limitScope = (querySelector("#limitScope") as CheckboxInputElement);
+final CheckboxInputElement limitScope =
+		(querySelector("#limitScope") as CheckboxInputElement);
 final RadioButtonInputElement radioScope0 = querySelector("#radioScope0");
 final RadioButtonInputElement radioScope1 = querySelector("#radioScope1");
 final RadioButtonInputElement radioScope2 = querySelector("#radioScope2");
-final List ltrs = ["A","B","C"];
+final List ltrs = ["A", "B", "C"];
 final Random rnd = new Random();
 int scope;
 
 void main() {
-
-		querySelector("#button").onClick.listen(simulate);
-
+	querySelector("#button").onClick.listen(simulate);
 }
 
 bool simulate(MouseEvent event) {
-
 	// Get number input from textfield.
 	int rounds = int.parse(numInput.value, onError: (e) => null);
 	// Exits execution if input is not an interger.
@@ -93,80 +91,61 @@ bool simulate(MouseEvent event) {
 	String outputText = "";
 
 	// Checks if user has requested to limit the scope of the simulation.
-	if (limitScope.checked) {updateScope();}
+	if (limitScope.checked) {
+		updateScope();
+	}
 
 	// Loops through randomized codes and tallies the number of steps
 	// necessary for each method to solve it, and keeps track of totals.
 	int j = rounds + 1;
 	for (int i = 1; i < j; i++) {
-
 		String code = "AAA";
 		if (limitScope.checked) {
-
 			// Randomizes a code until a desired one within the scope is found.
 			while (code.split("A").length != scope) {
-
 				code = rndCode();
-
 			}
 		} else {
-
 			code = rndCode();
-
 		}
 		int stpT = stepsT[code];
 		int stpM = stepsM[code];
 		stpTotalT = stpTotalT + stpT;
 		stpTotalM = stpTotalM + stpM;
-		outputText =
-				outputText +
+		outputText = outputText +
 				"\n\nRound ${i}: ${code}\n" +
 				"Thraxis:   ${stpT}, total: ${stpTotalT}\n" +
 				"Madison's: ${stpM}, total: ${stpTotalM}";
-
 	}
 
 	// Calculates average number of steps taken for each method.
-	outputText =
-			"Average steps\n" +
+	outputText = "Average steps\n" +
 			"-------------\n" +
 			"Thraxis:   ${stpTotalT / rounds}\n" +
 			"Madison's: ${stpTotalM / rounds}\n" +
-			"-------------" + outputText;
+			"-------------" +
+			outputText;
 
 	// Outputs results to webpage.
 	querySelector("#output").text = outputText;
-
 }
 
 // Updates the scope value based on user input.
 updateScope() {
-
 	if (radioScope0.checked) {
-
 		scope = int.parse(radioScope0.value);
-
 	} else if (radioScope1.checked) {
-
 		scope = int.parse(radioScope1.value);
-
 	} else {
-
 		scope = int.parse(radioScope2.value);
-
 	}
-
 }
 
 // Generates random code for our given methods to solve.
 String rndCode() {
-
 	String code = "";
 	for (int i = 0; i < 3; i++) {
-
 		code = code + ltrs[rnd.nextInt(3)];
-
 	}
 	return code;
-
 }
